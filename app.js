68a64d282ee3dabe142e97333f06cfc1c569cd11
Abdul Moiz Shoro <abdulmoizshoro@gmail.com>
2021-03-28 10:11:59 -0700

    work compelete

---
+++ app.js
@@ -0,0 +1,33 @@
+//ASSIGNMENT - 1
+
+//Destructuring of nested array
+
+// const array = ['moiz','waleed','huzaifa','anas','hamza'];
+// const [a , b , ...c] = array;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//Extract keys from object
+
+// const obj = {
+//     name: 'moiz',
+//     profession: 'Full Stack developer',
+//     experience:'5 year',
+//     age: 21,
+
+//   };
+//  console.log(Object.keys(obj));
+
+
+//Extract values from object
+
+// const obj = {
+//     name: 'moiz',
+//     profession: 'Full Stack developer',
+//     experience:'5 year',
+//     age: 21,
+
+//   };
+//  console.log(Object.values(obj));
